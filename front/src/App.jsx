cb07a6781c84b7bc53f62420a1aeadc40b37af2c
chanjin97 <chanjindark@naver.com>
2025-01-20 11:09:06 +0900

    provider 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -12,41 +12,38 @@ import CaseDetail from "./page/Building/CaseDetail.jsx";
 import FloorPlan from "./page/Building/FloorPlan.jsx";
 import Cctv from "./page/Building/Cctv.jsx";
 import Counting from "./page/Building/Counting.jsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
 
 function App() {
-  useEffect(() =>{
-    connectWebSocket();  // 컴포넌트가 마운트되면 웹소켓 연결
+  useEffect(() => {
+    connectWebSocket(); // 컴포넌트가 마운트되면 웹소켓 연결
   }, []);
-  
+
   return (
     <>
-      <Routes>
-        {/* 스타일 가이드 */}
-        {/* <Route path="/style" element={<Style />}></Route> */}
-
-        {/* 초기 경로 로그인으로 연결 */}
-        <Route path="/" element={<Navigate to="/login" />} />
-
-        {/* 로그인 페이지 */}
-        <Route path="/login" element={<Login />}></Route>
-
-        {/* 메인 페이지 */}
-        <Route path="/main" element={<Main />}></Route>
-
-        {/* 지도 페이지 */}
-        <Route path="/map" element={<Map />}></Route>
-
-        {/* 상세 페이지 */}
-        <Route path="/map/:bldgId" element={<CaseDetail />}></Route>
-        <Route path="/map/:bldgId/:flplanId" element={<FloorPlan />}></Route>
-
-        {/* CCTV 페이지 */}
-        <Route path="/cctv" element={<Cctv />}></Route>
-
-        {/* Counting 페이지 */}
-        <Route path="/counting" element={<Counting />}></Route>
-
-      </Routes>
+      {/* Redux를 사용하기위해 Provider 추가 */}
+      <Provider store={store}>
+        <Routes>
+          {/* 스타일 가이드 */}
+          {/* <Route path="/style" element={<Style />}></Route> */}
+          {/* 초기 경로 로그인으로 연결 */}
+          <Route path="/" element={<Navigate to="/login" />} />
+          {/* 로그인 페이지 */}
+          <Route path="/login" element={<Login />}></Route>
+          {/* 메인 페이지 */}
+          <Route path="/main" element={<Main />}></Route>
+          {/* 지도 페이지 */}
+          <Route path="/map" element={<Map />}></Route>
+          {/* 상세 페이지 */}
+          <Route path="/map/:bldgId" element={<CaseDetail />}></Route>
+          <Route path="/map/:bldgId/:flplanId" element={<FloorPlan />}></Route>
+          {/* CCTV 페이지 */}
+          <Route path="/cctv" element={<Cctv />}></Route>
+          {/* Counting 페이지 */}
+          <Route path="/counting" element={<Counting />}></Route>
+        </Routes>
+      </Provider>
     </>
   );
 }
--- front/src/index.jsx
+++ front/src/index.jsx
@@ -13,13 +13,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import 'mapbox-gl/dist/mapbox-gl.css'
+import "mapbox-gl/dist/mapbox-gl.css";
 import { Buffer } from "buffer";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
 
 window.Buffer = Buffer;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  // redux를 사용하기위해 Provider 추가
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
