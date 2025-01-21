9066d98794d2c03b9d34439ba54284f5f00b1f5b
seohyun4109 <seohyunkim4109@gmail.com>
2025-01-21 14:56:15 +0900

    2025.01.21-map+footer.jsx 프론트 기능 토글구현 추가

--- front/src/page/Map.jsx
+++ front/src/page/Map.jsx
@@ -400,7 +400,7 @@ function Map() {
       handleModalOrSearchChange(newModalState, "pin1");
     }
     if (pinType === "pin2") {
-      // 이미 마커가 있다면 제거하고 위치 추적 중지
+      // 이미 마커가 있다면 제거하고 위치 추적 중지(서현)
       if (currentLocationMarker) {
         currentLocationMarker.remove();
         setCurrentLocationMarker(null);
@@ -409,7 +409,6 @@ function Map() {
           setWatchId(null);
         }
         setActivePin(null);
-
         return;
       }
 
